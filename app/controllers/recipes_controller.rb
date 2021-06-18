5312a84a77c9db1e11ca68c867abfe3884d625fb
Ubuntu <ubuntu@ip-172-31-16-180.us-east-2.compute.internal>
2021-06-18 11:20:17 +0000

    recipegram.1

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,30 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end 
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
+  end 
+end
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '9ef69f41a5a7d3f09132089512baecdcf448daa247c2f524a0899001835cff813d2f381834d47a9679b2b2eca345d786bfb6bb65004fb38335f212fc89b664a9'
