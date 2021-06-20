9b44c708965be6166a410c248f42e428ccbbb7f8
Ubuntu <ubuntu@ip-172-31-16-180.us-east-2.compute.internal>
2021-06-20 07:55:09 +0000

    recipe post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
-  has_many :recipis, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "home#index"
-  resources :users
   resources :recipes
+  resources :users
 end
